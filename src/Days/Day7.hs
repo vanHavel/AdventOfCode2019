6ca676a6d3a0a7c4ff67f3f35314db2619a049a9
Lukas <dev.lukas.huwald@gmail.com>
2019-12-07 13:58:35 +0100

    day 7

--- src/Days/Day7.hs
+++ src/Days/Day7.hs
@@ -1,4 +1,4 @@
-module Days.Day5(run) where
+module Days.Day7(run) where
 
 import Control.Monad.ST
 import Data.Array
@@ -25,13 +25,41 @@ run :: String -> String
 run s = 
     let ints = map read $ splitOn "," s :: [Int]
         arr = listArray (0, pred $ length ints) ints
-        (output1, output2) = runST $ do
-            code <- thaw arr 
-            output1 <- compute code 0 [1]
+        perms1 = permutations [0, 1, 2, 3, 4]
+        outputs1 = map (runAmps arr 0) perms1
+        perms2 = permutations [5, 6, 7, 8, 9]
+        outputs2 = map (runAmpsFeedback arr) perms2
+    in show (maximum outputs1) ++ ", " ++ show (maximum outputs2)
+
+permutations :: [a] -> [[a]]
+permutations [] = [[]]
+permutations (x:xs) = do 
+    recursed <- permutations xs 
+    (left, right) <- partitions recursed
+    return $ left ++ (x:right)
+
+partitions :: [a] -> [([a], [a])]
+partitions [] = [([], [])]
+partitions l@(x:xs) = ([], l) : map (\(a, b) -> (x:a, b)) (partitions xs)
+
+runAmps :: Array Int Int -> Int -> [Int] -> Int 
+runAmps arr input [] = input
+runAmps arr input (signal:signals) = 
+    let [output] = runST $ do 
             code <- thaw arr
-            output2 <- compute code 0 [5]
-            return (output1, output2)
-    in show (last output1) ++ ", " ++ show (last output2)
+            compute code 0 [signal, input]
+    in runAmps arr output signals
+
+runAmpsFeedback :: Array Int Int -> [Int] -> Int 
+runAmpsFeedback arr signals = last amp5Output
+    where amp1Output = runWithInput $ [signals !! 0, 0] ++ amp5Output
+          amp2Output = runWithInput $ [signals !! 1] ++ amp1Output
+          amp3Output = runWithInput $ [signals !! 2] ++ amp2Output
+          amp4Output = runWithInput $ [signals !! 3] ++ amp3Output
+          amp5Output = runWithInput $ [signals !! 4] ++ amp4Output
+          runWithInput input = runST $ do 
+            code <- thaw arr 
+            compute code 0 input
 
 readParams :: STArray s Int Int -> Int -> Int -> [Access] -> ST s [Int]
 readParams _ _ _ [] = return []
--- src/Switcher.hs
+++ src/Switcher.hs
@@ -6,7 +6,7 @@ import Days.Day3
 import Days.Day4
 import Days.Day5
 import Days.Day6
---import Days.Day7
+import Days.Day7
 -- import Days.Day8
 -- import Days.Day9
 -- import Days.Day10
@@ -33,7 +33,7 @@ runDay 3 = Days.Day3.run
 runDay 4 = Days.Day4.run
 runDay 5 = Days.Day5.run
 runDay 6 = Days.Day6.run
---runDay 7 = Days.Day7.run
+runDay 7 = Days.Day7.run
 -- runDay 8 = Days.Day8.run
 -- runDay 9 = Days.Day9.run
 -- runDay 10 = Days.Day10.run
