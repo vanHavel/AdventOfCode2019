6a3c91e05e6ceadb164727797a242d7141e34276
Lukas <dev.lukas.huwald@gmail.com>
2019-12-22 10:52:11 +0100

    day 21 part 1

---
+++ src/Days/Day21.hs
@@ -0,0 +1,84 @@
+module Days.Day21(run) where 
+
+import Util.Intcode
+
+import Data.Array
+import Data.Char
+import Data.List.Split
+
+import Debug.Trace
+
+run :: String -> String 
+run s = 
+    let ints = (map read $ splitOn "," s)
+        code = listArray (0, pred $ length ints) ints
+        resultsWalk = [runProgram code $ map ord $ render script | script <- walkScripts]
+        damageWalk = last $ head $ filter (any (> 1000)) resultsWalk
+    in show damageWalk ++ ", "
+
+data Instruction = And Char Char | Or Char Char | Not Char Char | Walk | Run
+    deriving (Eq, Ord)
+type Script = [Instruction]
+
+instance Show Instruction where 
+    show (And c1 c2) = "AND" ++ ' ' : c1 : ' ' : c2 : "\n"
+    show (Or c1 c2) = "OR" ++ ' ' : c1 : ' ' : c2 : "\n"
+    show (Not c1 c2) = "NOT" ++ ' ' : c1 : ' ' : c2 : "\n"
+    show (Walk) = "WALK" ++ "\n"
+    show (Run) = "RUN" ++ "\n"
+
+render :: Script -> String 
+render = concatMap show
+
+walkScripts :: [Script] 
+walkScripts = do 
+    btct <- [True, False]
+    btcf <- [True, False]
+    bfct <- [True, False]
+    bfcf <- [True, False]
+    return $ funcScript [btct, btcf, bfct, bfcf] ++ baseScript Walk
+
+funcScript :: [Bool] -> Script 
+funcScript [False, False, False, False] = []
+funcScript [False, False, False, True] = [
+        Not 'B' 'T',
+        Not 'C' 'J', 
+        And 'T' 'J'
+    ]
+funcScript [False, False, True, False] = [
+        Not 'B' 'J',
+        And 'C' 'J'
+    ]
+funcScript [False, True, False, False] = [
+        Not 'C' 'J', 
+        And 'B' 'J'
+    ]
+funcScript [True, False, False, False] = [
+        Or 'B' 'J',
+        And 'C' 'J',
+        Not 'J' 'J'
+    ]
+funcScript [True, True, False, False] = [
+        Or 'B' 'J'
+    ]
+funcScript [True, False, False, True] = [
+        Or 'B' 'T',
+        And 'C' 'T',
+        Or 'B' 'J',
+        Or 'C' 'J',
+        Not 'J' 'J',
+        Or 'T' 'J'
+    ]
+funcScript [True, False, True, False] = [
+        Or 'C' 'J'
+    ]
+funcScript inputs = funcScript (map not inputs) ++ [Not 'J' 'J']
+
+
+baseScript :: Instruction -> Script 
+baseScript i = [
+        And 'D' 'J',
+        Not 'A' 'T',
+        Or 'T' 'J',
+        i
+    ]
